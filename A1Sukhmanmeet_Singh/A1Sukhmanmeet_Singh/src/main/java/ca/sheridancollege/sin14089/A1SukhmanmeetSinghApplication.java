8aa645acb51ab0eef9b18126610448bb3f5627ed
SukhmanmeetSingh <134074138+SukhmanmeetSingh@users.noreply.github.com>
2023-09-19 20:55:02 -0400

    Add files via upload
    File added

---
+++ A1Sukhmanmeet_Singh/A1Sukhmanmeet_Singh/src/main/java/ca/sheridancollege/sin14089/A1SukhmanmeetSinghApplication.java
@@ -0,0 +1,14 @@
+package ca.sheridancollege.sin14089;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1SukhmanmeetSinghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1SukhmanmeetSinghApplication.class, args);
+		System.out.println("Hello World from Sukhmanmeet Singh");
+	}
+
+}
